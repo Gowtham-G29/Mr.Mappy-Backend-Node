a517eebfd300284fa7d0fa95e8d5ce74dd00f167
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2024-12-10 00:13:51 +0530

    Update: Create the endpoints for the saome user middlewares

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,12 +11,16 @@ userRouter.post('/login', authController.login);
 //forgot password
 userRouter.post('/forgotPassword', authController.forgotPassword);
 //resetPassword
-userRouter.patch('/resetPassword/:token',authController.resetPassword);
+userRouter.patch('/resetPassword/:token', authController.resetPassword);
 
 //update current user Password
-userRouter.patch('/updateCurrentUserPassword',authController.protect,authController.updateCurrentUserPassword)
+userRouter.patch('/updateCurrentUserPassword', authController.protect, authController.updateCurrentUserPassword)
 
+//Upload profile image
+userRouter.patch('/updateMe', authController.protect, userController.uploadUserPhoto, userController.resizeUserPhoto,userController.updateMe);
 
+//delete or deactivate the account
+userRouter.patch('/deleteMe',authController.protect,userController.deleteMe);
 
 
 module.exports = userRouter;
