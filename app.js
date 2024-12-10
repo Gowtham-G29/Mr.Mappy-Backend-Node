6be32fa4ca645c002c656d94f1537919df056918
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2024-12-10 12:20:33 +0530

    Update : CORS

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express=require('express');
-const app=express();
-const morgan=require('morgan')
-const rateLimit=require('express-rate-limit') ;
-const helmet=require('helmet');
-const mongoSanitize=require('express-mongo-sanitize');
-const xss=require('xss-clean');
-const hpp=require('hpp');
-const compression=require('compression');
-const cors=require('cors');//prevent cross origin requests
-const cookieParser=require('cookie-parser');// get the token form the cookie
+const express = require('express');
+const app = express();
+const morgan = require('morgan')
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const compression = require('compression');
+const cors = require('cors');//prevent cross origin requests
+const cookieParser = require('cookie-parser');// get the token form the cookie
 
 //Middleware for logging in development 
-if(process.env.NODE_ENV=='development'){
+if (process.env.NODE_ENV == 'development') {
     app.use(morgan('dev'));
 }
 
 //Import routes
-const userRouter=require('./routes/userRoutes');
-const activityRouter=require('./routes/activityRoutes');
+const userRouter = require('./routes/userRoutes');
+const activityRouter = require('./routes/activityRoutes');
 
 
 //Global middleware to parse JSON request bodies
@@ -25,14 +25,17 @@ app.use(express.json());
 
 
 //Global middleware for protection
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',  // Your frontend's URL (update if using another URL)
+    credentials: true
+}));
 app.use(helmet());
-const limiter=rateLimit({
-    max:100,
-    windowMs:60*60*1000,
-    message:'Too many request from this IP . Please try again in an hour !'
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many request from this IP . Please try again in an hour !'
 })
-app.use('/api',limiter);
+app.use('/api', limiter);
 app.use(mongoSanitize());
 app.use(xss());
 app.use(hpp());
@@ -42,8 +45,8 @@ app.use(compression());
 
 
 //Use the route as Middlewares
-app.use('/api/v1/users',userRouter);
-app.use('/api/v1/activities',activityRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/activities', activityRouter);
 
 
 
@@ -51,26 +54,26 @@ app.use('/api/v1/activities',activityRouter);
 
 
 // error handling unhandled routes
-app.all('*',(req,res)=>{
+app.all('*', (req, res) => {
     res.status(404).json({
-        status:'fail',
-        message:`Can't find the ${req.originalUrl} on this server!`
+        status: 'fail',
+        message: `Can't find the ${req.originalUrl} on this server!`
     });
 });
 
 
 
 //Global error handling middleware
-app.use((err,req,res,next)=>{
-    err.statusCode=err.statusCode||500;
-    err.status=err.status||'Error';
-    
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'Error';
+
     //sending response
     res.status(err.statusCode).json({
-        status:err.status,
-        message:err.message
+        status: err.status,
+        message: err.message
     });
 })
 
 
-module.exports=app;
+module.exports = app;
