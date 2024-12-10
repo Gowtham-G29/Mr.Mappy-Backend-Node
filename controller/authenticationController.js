cb8397e84e5887c0087be027edd6904f95341b4e
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2024-12-10 13:36:49 +0530

    Update: cookie option

--- controller/authenticationController.js
+++ controller/authenticationController.js
@@ -26,7 +26,7 @@ exports.signUp = async (req, res, next) => {
         const cookieOptions = {
             expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
             secure: true,  //only need for Production
-            httpOnly: true
+            httpOnly: false
         }
         res.cookie('jwt', token, cookieOptions);
 
