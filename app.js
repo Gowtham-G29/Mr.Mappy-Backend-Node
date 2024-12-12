49f3f511a6e83dbc415ebb6def93335c684bb345
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2024-12-12 15:38:24 +0530

    fix: disable cors

--- app.js
+++ app.js
@@ -41,10 +41,10 @@ app.use(compression());
 app.use(cookieParser());
 
 //Global middleware for protection
-app.use(cors({
-    origin: 'http://localhost:5173',  // Your frontend's URL (update if using another URL)
-    credentials: true
-}));
+// app.use(cors({
+//     origin: 'http://localhost:5173',  // Your frontend's URL (update if using another URL)
+//     credentials: true
+// }));
 
 
 //Use the route as Middlewares
